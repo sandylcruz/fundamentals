6c01b9747371860e69b8926b0f2538afb84b46ba
Sandy Cruz <sandyleahcruz@icloud.com>
2020-04-18 14:20:51 -0700

    Write method to reverse characters not word order

--- w1/w1d2/exercises/lib/00_silly_blocks.rb
+++ w1/w1d2/exercises/lib/00_silly_blocks.rb
@@ -1,12 +1,32 @@
-def reverser
-  string = yield
-  reversed = string.reverse
-  reversed
+# def reverser(&prc)
+#   string = prc.call
+#   words = string.split(' ')
+#   accumulator = []
+#
+#   words.each do |word|
+#     accumulator << word.reverse
+#   end
+#
+#   accumulator.join(' ')
+# end
+
+def reverser(&prc)
+  string = prc.call
+  words = string.split(' ')
+
+  reverse_array = words.map do |word|
+    word.reverse
+  end
+
+  reverse_array.join(' ')
+
 end
 
-def adder
-  block + "#{number}"
+
+def adder(surplus = 1, &prc)
+  prc.call + surplus
 end
 
-def repeater
+def repeater(times = 1, &prc)
+  prc.call
 end
